401f8d3047cf7f8d4efe3059eabf6bd609037c8a
Butawantemi <japhetbuta@gmail.com>
2024-07-11 11:11:52 +0300

    add form to qr-code gerator

--- qr-code-generator/src/App.jsx
+++ qr-code-generator/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {QRCodeSVG} from 'qrcode.react';
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState(' ')
+  const [qrCode, setQrCode] = useState(' ')
+  const handleSubmit = (e) => {
+
+  }
+ 
 
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <div className='justify-items-center'>
+     <h1 className="text-3xl font-bold justify-center">
+      QR Code Generator
     </h1>
-    </>
+    <form>
+      <input type="url" name="url" value={url} onChange={(e) => setUrl(e.target.value)} placeholder='Enter URL' className='border-2 border-gray-300 p-2 m-2 border-r-amber-50' />
+      <button type='submit' onClick={handleSubmit} className='bg-blue-500 text-white p-2 m-2'>Generate QRCode</button>
+    </form>
+    
+    </div>
   )
 }
 
